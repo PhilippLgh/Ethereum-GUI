b4a5ab08b3dd177c110b9cb6ce9fdc9d8aa6015e
PhilippLgh <philipp@ethereum.org>
2020-04-16 13:45:19 +0200

    render connected peers as tree

--- src/components/main/network/Network.js
+++ src/components/main/network/Network.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
 import Container from '../../../widgets/Container'
+import Tree from '../../../widgets/Tree'
+import Error from '../../../widgets/Error'
 
 export default class Network extends Component {
+  state = {
+    networkData: undefined,
+    error: undefined
+  }
+  componentDidMount = async () => {
+    const { provider } = this.props
+    // const modules = await provider.send('rpc_modules', [])
+    // console.log('modules', modules)
+    const root = {
+      name: 'This Computer',
+      children: []
+    }
+    try {
+      const peers = await provider.send('admin_peers')
+      console.log('peers', peers)
+
+      for (const peer of peers) {
+        const {
+          name, // client-version-info
+          id,
+          enode,
+          enr,
+          protocol,
+          network
+        } = peer
+        root.children.push({
+          name,
+          attributes: network,
+          children: []
+        })
+      }
+
+    } catch (error) {
+      console.log('error', error)
+      return this.setState({
+        error
+      })
+    }
+    this.setState({
+      networkData: root
+    })
+  }
   render() {
+    const { networkData, error } = this.state
     return (
       <Container>
         <h2>Network</h2>
+        {networkData && <Tree
+          data={networkData}
+          orientation="horizontal"
+          pathFunc="straight"
+          centered="vertically"
+        />}
+        <Error error={error} />
       </Container>
     )
   }
