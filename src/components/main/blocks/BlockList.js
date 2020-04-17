e06d621393d56d6b2a23a569abb1ac95f2c78667
PhilippLgh <philipp@ethereum.org>
2020-04-17 13:04:55 +0200

    move item loading logic into list

--- src/components/main/blocks/BlockList.js
+++ src/components/main/blocks/BlockList.js
@@ -4,28 +4,18 @@ import List from '../../../widgets/List'
 import { getBlocks } from '../../../js/utils'
 
 export default class BlockList extends Component {
-  state = {
-    blocks: []
-  }
-  componentDidMount = async () => {
-    let { provider, start, end } = this.props
-    console.log('get blocks for range', start, end)
-    end = 100
-    this.setState({
-      isLoading: true
-    })
-    const blocks = await getBlocks(provider, end, Math.max(start, 0))
-    this.setState({
-      blocks: [...blocks],
-      isLoading: false
-    })
-  }
   render() {
-    let { blocks, isLoading } = this.state
+    let { provider, start, end } = this.props
+    end = 100
     return (
-      <List className="BlockList" isLoading={isLoading} >
-        { blocks.map(block => <Block key={block.number} block={block} />) }
-      </List>
+      <List 
+        className="BlockList"
+        itemName="Blocks"
+        loadItems={
+          () => getBlocks(provider, end, Math.max(start, 0))
+        }
+        renderItem={ block => <Block key={block.number} block={block} /> }
+      />
     )
   }
 }
--- src/widgets/List.js
+++ src/widgets/List.js
@@ -1,18 +1,95 @@
 import React, { Component } from 'react'
 import Pagination from './Pagination'
-import Container from './Container'
+import Spinner from './Spinner'
+import Error from './Error'
+
+// TODO implement list header
+class ListHeader extends Component { }
 
 export default class List extends Component {
-  render() {
-    const { children, elements } = this.props
+  state = {
+    items: [],
+    isLoading: false,
+    error: undefined
+  }
+  componentDidMount = async () => {
+    const { loadItems } = this.props
+    if (loadItems) {
+      this.setState({
+        isLoading: true
+      })
+      try {
+        let items = await loadItems()
+        // filter null and undefined
+        items = items.filter(item => !!item)
+        this.setState({
+          items,
+          isLoading: false
+        })
+      } catch (error) {
+        console.log('loading error', error)
+        this.setState({
+          error,
+          isLoading: false
+        })
+      }
+    }
+  }
+  renderLoading() {
     return (
-      <Container>
+      <div style={{
+        maxHeight: 300,
+        display: 'flex',
+        flexDirection: 'column',
+        opacity: 0.6,
+        alignItems: 'center'
+      }}>
+        <div style={{
+          fontSize: '2rem',
+          alignSelf: 'center',
+          margin: 20
+        }}>Loading items...</div>
+       <Spinner style={{
+         width: 50,
+         height: 50
+       }} />
+      </div>
+    )
+  }
+  renderItems() {
+    const { items } = this.state
+    const { style, renderItem, itemName = "items" } = this.props
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto',
+      }}>
+        { items.length > 0
+          ? items.map(renderItem)
+          : <h2 style={{ alignSelf: 'center', color: '#ccc' }}>No {itemName} found</h2>
+        }
+      </div>
+    )
+  }
+  render() {
+    const { isLoading, items, error } = this.state
+    const { elements } = this.props
+    return (
+      <div style={{
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+      }}>
         {
           elements && elements()
         }
-        { Array.isArray(children) && children.length > 25 && <Pagination /> }
-        { children }
-      </Container>
+        {items.length > 25 && <Pagination />}
+        {error
+        ? <Error error={error}/>
+        : isLoading ? this.renderLoading() : this.renderItems()
+        }
+      </div>
     )
   }
 }
