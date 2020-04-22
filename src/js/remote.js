783949b2be0a4b01525b652bbb8a4cfd0ecfc61f
PhilippLgh <philipp@ethereum.org>
2020-04-22 11:04:39 +0200

    handle remote call args

--- src/js/remote.js
+++ src/js/remote.js
@@ -3,18 +3,22 @@ export const remote = (worker) => {
   let id = 0
   return new Proxy({}, {
     get(target, prop) {
-      return () => new Promise((resolve, reject) => {
+      return (...args) => new Promise((resolve, reject) => {
         const requestId = id
         worker.postMessage({
           id: id++,
-          method: prop
+          method: prop,
+          arguments: [...args]
         })
         let responseHandler = function (event) {
           const { data } = event
-          const { result, id: responseId } = data
+          const { result, id: responseId, error } = data
           if (requestId === responseId) {
-            resolve(result)
             worker.removeEventListener('message', this)
+            if (error) {
+              return reject(error)
+            }
+            resolve(result)
           }
         }
         worker.addEventListener('message', responseHandler)
