a0a6b3cada2c1c30ccee8b491edbe00a972dcfec
PhilippLgh <philipp@ethereum.org>
2020-04-22 11:29:40 +0200

    Updates

---
+++ precache-manifest.2dcd09b318669d4edc6fce6d90434b3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df776707ee5b30c6ed5c6c23c9f36618",
+    "url": "/ethereum-gui/0eb0363c21cede2e29f4.worker.js"
+  },
+  {
+    "revision": "5b34ecd2bb2fa004ab9f8472595f3636",
+    "url": "/ethereum-gui/0eb0363c21cede2e29f4.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "f170bf1d3a664fa57bd18e3faa2c112b",
+    "url": "/ethereum-gui/index.html"
+  },
+  {
+    "revision": "23813901a6caa34d9577",
+    "url": "/ethereum-gui/static/css/main.952301f1.chunk.css"
+  },
+  {
+    "revision": "1629ed3e3e85e0e6526d",
+    "url": "/ethereum-gui/static/js/2.7abbef32.chunk.js"
+  },
+  {
+    "revision": "89dad59ef2b8f7334350559b285a49c6",
+    "url": "/ethereum-gui/static/js/2.7abbef32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23813901a6caa34d9577",
+    "url": "/ethereum-gui/static/js/main.1fa63549.chunk.js"
+  },
+  {
+    "revision": "25862efa8f50860f3ae3",
+    "url": "/ethereum-gui/static/js/runtime-main.dfe66ad8.js"
+  }
+]);
