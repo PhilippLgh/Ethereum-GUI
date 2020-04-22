84728eac3bc59a2736209067c9ecee4fb1f52b04
PhilippLgh <philipp@ethereum.org>
2020-04-22 11:07:04 +0200

    remove data provider logic from utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,58 +1,20 @@
 import { ethers } from "ethers"
+import Storage from "./Storage"
 
-export const getBlocks = async (provider, end, start=0) => {
-  const blocks = []
-  let i = Math.max(start, 0)
-  while (i <= end) {
-    const block = await provider.getBlock(i)
-    blocks.push(block)
-    i++
+export const parseTransaction = (abi, tx) => {
+  // TODO memoize
+  const _interface = new ethers.utils.Interface(abi)
+  const parsed = _interface.parseTransaction(tx)
+  return parsed
+}
+
+export const tryParseTransaction = (tx, abi) => {
+  // TODO try to do abi or hash lookup by tx.data, tx address or bytecode
+  // TODO is bug? parsed constructor = null
+  const parsed = parseTransaction(abi, tx)
+  return parsed ? parsed : {
+    name: 'constructor'
   }
-  return blocks
-}
-
-const getAllTransactionForBlock = async (provider, block) => {
-  if (!block) return []
-  const { transactions: txHashes } = block
-  const transactions = []
-  for (const txHash of txHashes) {
-    const transaction = await provider.getTransaction(txHash)
-    transactions.push(transaction)
-  }
-  return transactions
-}
-
-export const getAllTransactions = async (provider) => {
-  const latestBlock = await provider.getBlockNumber()
-  const blocks = await getBlocks(provider, latestBlock)
-  const transactions = []
-  for (const block of blocks) {
-    const tx = await getAllTransactionForBlock(provider, block)
-    transactions.push(...tx)
-  }
-  return transactions
-}
-
-export const getAllTxByContract = async (provider, contractAddress) => {
-  const allTx = await getAllTransactions(provider)
-  const contractTx = allTx.filter(tx => tx.to === contractAddress || tx.creates === contractAddress)
-  return contractTx
-}
-
-export const getAllContracts = async (provider) => {
-  const transactions = await getAllTransactions(provider)
-  const contractCreations = transactions.filter(tx => tx.creates).map(tx => tx.hash)
-  const receipts = []
-  for (const txHash of contractCreations) {
-    const receipt = await provider.getTransactionReceipt(txHash)
-    receipts.push(receipt)
-  }
-  return receipts
-}
-
-export const getAllAddresses = async (provider) => {
-  const accounts = await provider.listAccounts()
-  return [...accounts]
 }
 
 export const createFundedTestAccount = async (provider) => {
@@ -78,18 +40,9 @@ export const createFundedTestAccount = async (provider) => {
   return tx
 }
 
-const code = `
-contract Storage {
-  uint pos0;
-  mapping(address => uint) pos1;
-  
-  function Storage() {
-      pos0 = 1234;
-      pos1[msg.sender] = 5678;
-  }
-}
-`
 
-export const getSourceCode = async (contractAddressOrFileName) => {
-  return code
+export const getCompilerData = async (bytecode) => {
+  const storage = await Storage.getInstance()
+  return undefined
+  // return storage.get(bytecode)
 }
