b55b95377f5e5bb443331a9343dd9d7e47b942d2
PhilippLgh <philipp@ethereum.org>
2020-04-22 10:54:40 +0200

    add TextField

---
+++ src/widgets/TextField.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+export default class TextField extends Component {
+  render() {
+    const { label, value, onChange, suffix, disabled=false, required=false } = this.props
+    return (
+      <div style={{
+        alignItems: 'center',
+        margin: 10
+      }}>
+        <label
+          style={{
+            fontSize: '1rem'
+          }}
+          data-shrink="true"
+          htmlFor="standard-required"
+          id="standard-required-label">
+          {label}
+          { required && <span>*</span> }
+        </label>
+        <div
+          style={{
+            marginTop: 5,
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <input
+            disabled={disabled}
+            size="50"
+            style={{
+              fontSize: 'inherit',
+              boxSizing: 'content-box',
+              background: 'none',
+              margin: 0,
+              padding: 10,
+              font: 'inherit',
+              height: '1.2rem',
+              border: '1px solid gray',
+              borderRadius: 3
+            }}
+            type="text"
+            aria-invalid="false"
+            id="standard-required"
+            required=""
+            value={value}
+            onChange={e => onChange(e.target.value)}
+          />
+          { suffix && <span style={{ padding: 5, opacity: 0.7 }}>{suffix}</span>}
+        </div>
+      </div>
+    )
+  }
+}
